f7d7fc74be0bb9ee8673bd86abb0eb08c7891d4f
tomato-nit <65883963+tomato-nit@users.noreply.github.com>
2020-07-15 19:50:36 +0900

    コメントアウトの削除

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,25 +1,18 @@
 $(function(){ 
     var reloadMessages = function() {
-    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
     var last_message_id = $('.message:last').data("message-id");
     $.ajax({
-      //ルーティングで設定した通りのURLを指定
       url: "api/messages",
-      //ルーティングで設定した通りhttpメソッドをgetに指定
       type: 'get',
       dataType: 'json',
-      //dataオプションでリクエストに値を含める
       data: {id: last_message_id}
     })
     .done(function(messages) {
       if (messages.length !== 0) {
-      //追加するHTMLの入れ物を作る
       var insertHTML = '';
-      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
       $.each(messages, function(i, message) {
         insertHTML += buildHTML(message)
       });
-      //メッセージが入ったHTMLに、入れ物ごと追加
       $('.messages').append(insertHTML);
       $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
       }
@@ -30,7 +23,6 @@ $(function(){
   };
   function buildHTML(message){
     if ( message.image ) {
-        //data-idが反映されるようにしている
         var html =
          `<div class="message" data-message-id=${message.id}>
             <div class="upper-message">
@@ -50,7 +42,6 @@ $(function(){
           </div>`
         return html;
       } else {
-        //同様にdata-idが反映されるようにしている
         var html =
          `<div class="message" data-message-id=${message.id}>
             <div class="upper-message">
